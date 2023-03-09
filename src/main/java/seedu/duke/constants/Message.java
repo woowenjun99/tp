15ec2b64481c0f247047d908f8f42fba397d2d89
woowenjun99 <woowenjun99@gmail.com>
2023-03-09 10:09:31 +0800

    Add in a welcome message

--- src/main/java/seedu/duke/constants/Message.java
+++ src/main/java/seedu/duke/constants/Message.java
@@ -4,4 +4,15 @@ package seedu.duke.constants;
  * Contains the messages that will be printed by the ui chatbot
  */
 public enum Message {
+    WELCOME("Welcome");
+
+    Message(String message) {
+        this.message = message;
+    }
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
 }

1714bd8bf60b7f0872de05230458c2ca1f32b5ee
woowenjun99 <woowenjun99@gmail.com>
2023-03-09 10:14:08 +0800

    Add in the goodbye message to the Message enum

--- src/main/java/seedu/duke/constants/Message.java
+++ src/main/java/seedu/duke/constants/Message.java
@@ -4,7 +4,8 @@ package seedu.duke.constants;
  * Contains the messages that will be printed by the ui chatbot
  */
 public enum Message {
-    WELCOME("Welcome");
+    GOODBYE("Thank you for using MoneyMoover! We hope to see you again soon :)"),
+    WELCOME("Welcome to MoneyMoover!");
 
     Message(String message) {
         this.message = message;

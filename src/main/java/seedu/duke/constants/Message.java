51103f74cec6afcab388dd934276c8ff796969fa
woowenjun99 <woowenjun99@gmail.com>
2023-03-09 10:39:24 +0800

    Update the Message enum attribute to pass the CI

--- src/main/java/seedu/duke/constants/Message.java
+++ src/main/java/seedu/duke/constants/Message.java
@@ -4,14 +4,14 @@ package seedu.duke.constants;
  * Contains the messages that will be printed by the ui chatbot
  */
 public enum Message {
+
     GOODBYE("Thank you for using MoneyMoover! We hope to see you again soon :)"),
     WELCOME("Welcome to MoneyMoover!");
+    private String message;
 
     Message(String message) {
         this.message = message;
     }
-
-    private String message;
 
     public String getMessage() {
         return message;

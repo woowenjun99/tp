9d07a373b1fa8ff2a974e7a222b6f857b1e312dc
Arif-Khalid <arifkhalid99@gmail.com>
2023-03-09 11:19:14 +0800

    Add AccountList class with critical functions

--- src/main/java/seedu/duke/Account.java
+++ src/main/java/seedu/duke/Account.java
@@ -4,24 +4,71 @@ public class Account {
     private int balance;
     private Currency currency;
 
-    public int getBalance(){
-        return balance;
+    public float getBalance(){
+        return balance / 100.0f;
     }
     public Currency getCurrencyType(){
         return currency;
     }
-    public void updateBalance(int changeInBalance){
-        int newBalance = balance - changeInBalance;
+    public void updateBalance(float changeInBalance){
+        int newBalance = balance - (int)(changeInBalance * 100);
         if(newBalance < 0){
             // todo throw some exception
         }
         balance = newBalance;
     }
 
+    private static String currencyToString(Currency currency){
+        String currencyType;
+        switch (currency){
+        case SGD:
+            currencyType = "SGD";
+            break;
+        case USD:
+            currencyType = "USD";
+            break;
+        case CNY:
+            currencyType = "CNY";
+            break;
+        case EUR:
+            currencyType = "EUR";
+            break;
+        case GBP:
+            currencyType = "GBP";
+            break;
+        case IDR:
+            currencyType = "IDR";
+            break;
+        case JPY:
+            currencyType = "JPY";
+            break;
+        case KRW:
+            currencyType = "KRW";
+            break;
+        case MYR:
+            currencyType = "MYR";
+            break;
+        case THB:
+            currencyType = "THB";
+            break;
+        case VND:
+            currencyType = "VND";
+            break;
+        default:
+            // todo throw some error for undefined or unhandled currency type
+            currencyType = "";
+        }
+        return currencyType;
+    }
+    @Override
+    public String toString(){
+        String currencyType = currencyToString(currency);
+        return currencyType + ": " + Float.toString(balance / 100.0f);
+    }
+
     Account(Currency currency, int initialBalance){
         this.currency = currency;
         balance = initialBalance;
     }
-
 
 }

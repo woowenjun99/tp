f1c2f96affb69751a9ca4b99d7f9785dd530e735
woowenjun99 <woowenjun99@gmail.com>
2023-03-12 13:18:36 +0800

    Write test for processCommand

--- src/main/java/seedu/duke/commands/BalanceCommand.java
+++ src/main/java/seedu/duke/commands/BalanceCommand.java
@@ -1,12 +1,30 @@
 package seedu.duke.commands;
 
+import seedu.duke.AccountList;
+import seedu.duke.Currency;
+
 public class BalanceCommand extends Command {
-    BalanceCommand() {
+    private AccountList accounts = AccountList.getInstance();
+    private String command;
+
+
+    private String processCommand() {
+        String[] words = command.split(" ");
+        if (words.length == 1) {
+            return "ALL";
+        }
+        return words[1];
+    }
+
+    BalanceCommand(String command) {
         super(false);
+        this.command = command.trim();
     }
 
     @Override
     public void execute() {
-        
+        // Process command
+        // Get currency
+        // Print output
     }
 }
--- src/test/java/seedu/duke/commands/BalanceCommandTest.java
+++ src/test/java/seedu/duke/commands/BalanceCommandTest.java
@@ -2,16 +2,22 @@ package seedu.duke.commands;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+
 public class BalanceCommandTest {
     @Test
-    public void getBalance_NoAccount_shouldThrowError() {
-    }
-
-    @Test
-    public void getBalance_ListOneAccount_correctAccountListed() {
-    }
-
-    @Test
-    public void getBalance_NeverSpecifyAccount_allAccountsListed() {
+    public void processCommand_NoCurrencySpecified_ShouldReturnAll() {
+        try {
+            Method method = BalanceCommand.class.getDeclaredMethod("processCommand");
+            method.setAccessible(true);
+            BalanceCommand command = new BalanceCommand("balance");
+            assertEquals("ALL", method.invoke(command));
+        } catch (Exception e) {
+            fail();
+        }
     }
 }

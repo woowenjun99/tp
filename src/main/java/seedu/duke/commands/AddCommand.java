613786fa60d68bff0046a90648a931d95ebf2fd3
woowenjun99 <woowenjun99@gmail.com>
2023-03-12 23:33:12 +0800

    Set up template for AddCommand

---
+++ src/main/java/seedu/duke/commands/AddCommand.java
@@ -0,0 +1,14 @@
+package seedu.duke.commands;
+
+import seedu.duke.ui.Ui;
+
+public class AddCommand extends Command {
+    public AddCommand(String input) {
+        super(false, input);
+    }
+
+    @Override
+    public void execute(Ui ui) {
+
+    }
+}

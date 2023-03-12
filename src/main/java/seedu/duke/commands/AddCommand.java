dc72ed8a065ef1b6a38d790206f04993a2742d46
woowenjun99 <woowenjun99@gmail.com>
2023-03-12 23:44:37 +0800

    Write processCommand and getCurrency function

--- src/main/java/seedu/duke/commands/AddCommand.java
+++ src/main/java/seedu/duke/commands/AddCommand.java
@@ -1,10 +1,25 @@
 package seedu.duke.commands;
 
+import seedu.duke.Currency;
+import seedu.duke.exceptions.InvalidAddCommandException;
 import seedu.duke.ui.Ui;
 
 public class AddCommand extends Command {
     public AddCommand(String input) {
         super(false, input);
+    }
+
+    private Currency getCurrency(String currencyString) throws IllegalArgumentException {
+        return Currency.valueOf(currencyString);
+    }
+
+    private void processCommand() throws InvalidAddCommandException {
+        String[] words = input.split(" ");
+        // Format: [Command, CURRENCY, AMOUNT]
+        boolean isValidCommand = words.length == 3;
+        if (!isValidCommand) {
+            throw new InvalidAddCommandException();
+        }
     }
 
     @Override

16d94de6a08808273b9e3a441ae1e74e7c8ee9a4
woowenjun99 <woowenjun99@gmail.com>
2023-03-13 00:02:26 +0800

    Write function to parse currency as enum

--- src/test/java/seedu/duke/commands/AddCommandTest.java
+++ src/test/java/seedu/duke/commands/AddCommandTest.java
@@ -1,10 +1,12 @@
 package seedu.duke.commands;
 
 import org.junit.jupiter.api.Test;
+import seedu.duke.Currency;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.fail;
 
@@ -19,6 +21,18 @@ public class AddCommandTest {
             assertThrows(InvocationTargetException.class, () -> {
                 method.invoke(command, "JP");
             });
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void getCurrency_validCurrencyProvided_shouldReturnCorrespondingCurrency() {
+        try {
+            Method method = AddCommand.class.getDeclaredMethod("getCurrency", String.class);
+            method.setAccessible(true);
+            AddCommand command = new AddCommand("add JPY 200");
+            assertEquals(Currency.JPY, method.invoke(command, "JPY"));
         } catch (Exception e) {
             fail();
         }

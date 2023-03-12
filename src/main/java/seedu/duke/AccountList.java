66a40d11a3e6c002abda37e680ffcaed2fab1fb0
woowenjun99 <woowenjun99@gmail.com>
2023-03-12 12:16:32 +0800

    Make AccountList class singleton

--- src/main/java/seedu/duke/AccountList.java
+++ src/main/java/seedu/duke/AccountList.java
@@ -5,27 +5,36 @@ import java.util.HashMap;
 public class AccountList {
     // Currency implementation only specifies one account per currency, if required would have to change to a
     // Hashmap of ArrayList of account in the future and randomly generate an ID for that account
-    private HashMap<Currency, Account> accountHashMap;
-    AccountList(){
+    private final HashMap<Currency, Account> accountHashMap;
+    private static AccountList instance;
+
+    private AccountList() {
         accountHashMap = new HashMap<>();
     }
-    
-    public void addAccount(Currency currency, int initialBalance){
-        if(accountHashMap.containsKey(currency)){
+
+    public static AccountList getInstance() {
+        if (instance == null) {
+            instance = new AccountList();
+        }
+        return instance;
+    }
+
+    public void addAccount(Currency currency, int initialBalance) {
+        if (accountHashMap.containsKey(currency)) {
             // todo throw some exception signalling account already exists
         }
         accountHashMap.put(currency, new Account(initialBalance, currency));
     }
 
-    public void deleteAccount(Currency currency){
-        if(!accountHashMap.containsKey(currency)){
+    public void deleteAccount(Currency currency) {
+        if (!accountHashMap.containsKey(currency)) {
             // todo throw some exception signalling account already exists
         }
         accountHashMap.remove(currency);
     }
 
-    public float getBalance(Currency currency){
-        if(!accountHashMap.containsKey(currency)){
+    public float getBalance(Currency currency) {
+        if (!accountHashMap.containsKey(currency)) {
             // todo throw some exception signalling no such account
         }
         return accountHashMap.get(currency).getBalance();

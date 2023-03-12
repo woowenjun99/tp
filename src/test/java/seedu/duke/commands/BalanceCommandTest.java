e95812a3e73ee935e7e791f3ee46f23110e1cb8d
woowenjun99 <woowenjun99@gmail.com>
2023-03-12 12:07:29 +0800

    Set up test template for balance command

---
+++ src/test/java/seedu/duke/commands/BalanceCommandTest.java
@@ -0,0 +1,17 @@
+package seedu.duke.commands;
+
+import org.junit.jupiter.api.Test;
+
+public class BalanceCommandTest {
+    @Test
+    public void getBalance_NoAccount_shouldThrowError() {
+    }
+
+    @Test
+    public void getBalance_ListOneAccount_correctAccountListed() {
+    }
+
+    @Test
+    public void getBalance_NeverSpecifyAccount_allAccountsListed() {
+    }
+}

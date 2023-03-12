5ac1568a66d7415e7cde653943389f3fc6efed85
woowenjun99 <woowenjun99@gmail.com>
2023-03-13 00:30:24 +0800

    Add in an invalid numeric amount error message

--- src/main/java/seedu/duke/constants/ErrorMessage.java
+++ src/main/java/seedu/duke/constants/ErrorMessage.java
@@ -4,8 +4,10 @@ package seedu.duke.constants;
  * A public interface that is used to contain all the error messages throughout the application.
  */
 public interface ErrorMessage {
-    String INVALID_ADD_COMMAND = "Please check that you have provided the input in the following format: add <Currency> <Amount>";
+    String INVALID_ADD_COMMAND = "Please check that you have provided the input in the " +
+            "following format: add <Currency> <Amount>";
     String INVALID_CURRENCY = "An invalid currency has been provided.";
+    String INVALID_NUMERICAL_AMOUNT = "Please provide a numerical amount";
     String MORE_THAN_ONE_CURRENCY_PROVIDED = "Please do not provide more than one currency.";
     String NO_SUCH_ACCOUNT = "You do not have an account for the currency.";
     String INVALID_EXCHANGE_ARGUMENT = "Please structure your exchange as 'exchange STARTING_CURRENCY TARGET_CURRENCY AMOUNT_IN_STARTING'";

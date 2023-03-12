e401cb211e287bfc133e18065451295e147622fc
woowenjun99 <woowenjun99@gmail.com>
2023-03-13 00:28:07 +0800

    Add a invalid add command error

--- src/main/java/seedu/duke/constants/ErrorMessage.java
+++ src/main/java/seedu/duke/constants/ErrorMessage.java
@@ -4,6 +4,7 @@ package seedu.duke.constants;
  * A public interface that is used to contain all the error messages throughout the application.
  */
 public interface ErrorMessage {
+    String INVALID_ADD_COMMAND = "Please check that you have provided the input in the following format: add <Currency> <Amount>";
     String INVALID_CURRENCY = "An invalid currency has been provided.";
     String MORE_THAN_ONE_CURRENCY_PROVIDED = "Please do not provide more than one currency.";
     String NO_SUCH_ACCOUNT = "You do not have an account for the currency.";

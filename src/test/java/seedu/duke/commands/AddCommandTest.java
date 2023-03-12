282738e0bf45249aa9096b82ac49a6fe6521568c
woowenjun99 <woowenjun99@gmail.com>
2023-03-13 01:08:03 +0800

    Add in accountList instances to the test

--- src/test/java/seedu/duke/commands/AddCommandTest.java
+++ src/test/java/seedu/duke/commands/AddCommandTest.java
@@ -17,9 +17,10 @@ public class AddCommandTest {
     @Test
     public void getCurrency_invalidCurrencyProvided_shouldThrowException() {
         try {
+            AccountList account = new AccountList();            
             Method method = AddCommand.class.getDeclaredMethod("getCurrency", String.class);
             method.setAccessible(true);
-            AddCommand command = new AddCommand("add JPY 200");
+            AddCommand command = new AddCommand("add JPY 200", account);
             assertThrows(InvocationTargetException.class, () -> {
                 method.invoke(command, "JP");
             });
@@ -31,9 +32,10 @@ public class AddCommandTest {
     @Test
     public void getCurrency_validCurrencyProvided_shouldReturnCorrespondingCurrency() {
         try {
+            AccountList account = new AccountList();            
             Method method = AddCommand.class.getDeclaredMethod("getCurrency", String.class);
             method.setAccessible(true);
-            AddCommand command = new AddCommand("add JPY 200");
+            AddCommand command = new AddCommand("add JPY 200", account);
             assertEquals(Currency.JPY, method.invoke(command, "JPY"));
         } catch (Exception e) {
             fail();
@@ -43,9 +45,10 @@ public class AddCommandTest {
     @Test
     public void processCommand_commandLessThanThreeWords_shouldThrowException() {
         try {
+            AccountList account = new AccountList();            
             Method method = AddCommand.class.getDeclaredMethod("processCommand");
             method.setAccessible(true);
-            AddCommand command = new AddCommand("add JPY");
+            AddCommand command = new AddCommand("add JPY", account);
             assertThrows(InvocationTargetException.class, () -> {
                 method.invoke(command);
             });
@@ -57,9 +60,10 @@ public class AddCommandTest {
     @Test
     public void processCommand_amountNotInt_shouldThrowException() {
         try {
+            AccountList account = new AccountList();            
             Method method = AddCommand.class.getDeclaredMethod("processCommand");
             method.setAccessible(true);
-            AddCommand command = new AddCommand("add JPY m");
+            AddCommand command = new AddCommand("add JPY m", account);
             assertThrows(InvocationTargetException.class, () -> {
                 method.invoke(command);
             });
@@ -71,9 +75,10 @@ public class AddCommandTest {
     @Test
     public void processCommand_correctInputFormat_shouldNotThrowException() {
         try {
+            AccountList account = new AccountList();
             Method method = AddCommand.class.getDeclaredMethod("processCommand");
             method.setAccessible(true);
-            AddCommand command = new AddCommand("add JPY 200");
+            AddCommand command = new AddCommand("add JPY 200", account);
             assertDoesNotThrow(() -> {
                 method.invoke(command);
             });
@@ -85,9 +90,9 @@ public class AddCommandTest {
     @Test
     public void execute_correctInputProvided_shouldUpdateAmount() {
         try {
-            AccountList account = AccountList.getInstance();
+            AccountList account = new AccountList();
             account.addAccount(Currency.KRW, 2000);
-            AddCommand command = new AddCommand("add KRW 2000");
+            AddCommand command = new AddCommand("add KRW 2000", account);
             Ui ui = new Ui();
             command.execute(ui);
             int expectedAmount =  (int) account.getBalance(Currency.KRW).get(Currency.KRW).getBalance() * 100;

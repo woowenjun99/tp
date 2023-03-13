caaab9c31354184d8b75e19e4c9c6fcaf126e6d1
woowenjun99 <woowenjun99@gmail.com>
2023-03-13 15:35:59 +0800

    Add in the account list into exit command

--- src/main/java/seedu/duke/commands/ExitCommand.java
+++ src/main/java/seedu/duke/commands/ExitCommand.java
@@ -1,18 +1,18 @@
 package seedu.duke.commands;
 
-import seedu.duke.AccountList;
 import seedu.duke.ui.Ui;
+import seedu.duke.AccountList;
 
 /**
  * Command to exit the program
  */
 public class ExitCommand extends Command{
-    public ExitCommand(AccountList account) {
-        super(true, "", account);
+    public ExitCommand() {
+        super(true, "");
     }
 
     @Override
-    public void execute(Ui ui) {
+    public void execute(Ui ui, AccountList account) {
         ui.printFarewell();
     }
 }

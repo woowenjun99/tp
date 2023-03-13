79ee1e851bde284f541502ae186fefb9227f0fc9
woowenjun99 <woowenjun99@gmail.com>
2023-03-13 23:30:54 +0800

    Update the exception that the amount is non integer like string or float

--- src/main/java/seedu/duke/constants/ErrorMessage.java
+++ src/main/java/seedu/duke/constants/ErrorMessage.java
@@ -6,7 +6,7 @@ package seedu.duke.constants;
 public interface ErrorMessage {
     String INVALID_ADD_COMMAND = "Please check that you have correctly provided the currency and amount";
     String INVALID_CURRENCY = "An invalid currency has been provided.";
-    String INVALID_NUMERICAL_AMOUNT = "Please provide a numerical amount";
+    String INVALID_NUMERICAL_AMOUNT = "Please provide a an integer for the amount to update";
     String MORE_THAN_ONE_CURRENCY_PROVIDED = "Please do not provide more than one currency.";
     String NO_SUCH_ACCOUNT = "You do not have an account for the currency.";
 }

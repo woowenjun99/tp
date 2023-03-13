f6445f55ccace59768f31a4e73a4427a81cf3c3a
woowenjun99 <woowenjun99@gmail.com>
2023-03-14 00:43:51 +0800

    Update the INVALID_NUMERICAL_AMOUNT to float instead

--- src/main/java/seedu/duke/constants/ErrorMessage.java
+++ src/main/java/seedu/duke/constants/ErrorMessage.java
@@ -7,7 +7,7 @@ public interface ErrorMessage {
     String INVALID_ADD_COMMAND = "Please check that you have correctly provided the currency and amount";
     String INVALID_AMOUNT_TO_ADD = "Please provide a value greater than 0";
     String INVALID_CURRENCY = "An invalid currency has been provided.";
-    String INVALID_NUMERICAL_AMOUNT = "Please provide a an integer for the amount to update";
+    String INVALID_NUMERICAL_AMOUNT = "Please provide a float amount to update";
     String MORE_THAN_ONE_CURRENCY_PROVIDED = "Please do not provide more than one currency.";
     String NO_SUCH_ACCOUNT = "You do not have an account for the currency.";
     String NO_AMOUNT_PROVIDED = "Please provide an amount to be added.";

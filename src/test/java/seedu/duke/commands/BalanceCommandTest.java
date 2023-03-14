9d7f7498fa2276ae207f71b3fd72db636153ef18
Gan868611 <y.zganz.y@gmail.com>
2023-03-14 10:44:41 +0800

    Fix BalanceCommandTest

--- src/test/java/seedu/duke/commands/BalanceCommandTest.java
+++ src/test/java/seedu/duke/commands/BalanceCommandTest.java
@@ -90,8 +90,8 @@ public class BalanceCommandTest {
                     account
             );
             assertEquals(2, output.size());
-            assertEquals(200, 100 * output.get(Currency.CNY).getBalance());
-            assertEquals(40, 100 * output.get(Currency.EUR).getBalance());
+            assertEquals(200,  output.get(Currency.CNY).getBalance()/100);
+            assertEquals(40, output.get(Currency.EUR).getBalance()/100);
         } catch (Exception e) {
             fail();
         }

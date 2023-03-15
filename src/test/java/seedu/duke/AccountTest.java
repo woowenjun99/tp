387672a1aa678f11318923081de3ff079ce4c141
woowenjun99 <woowenjun99@gmail.com>
2023-03-15 19:37:41 +0800

    Add in test for update balance

---
+++ src/test/java/seedu/duke/AccountTest.java
@@ -0,0 +1,25 @@
+package seedu.duke;
+
+import org.junit.jupiter.api.Test;
+import seedu.duke.exceptions.TooLargeAmountException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+public class AccountTest {
+    @Test
+    public void updateBalance_exceedAmount_shouldThrowException () {
+        Account account = new Account(0, Currency.JPY);
+        assertThrows(TooLargeAmountException.class,
+                () -> account.updateBalance(1_000_000_001, "add")
+        );
+    }
+
+    @Test
+    public void updateBalance_validInput_shouldAddCorrectly () {
+        Account account = new Account(0, Currency.JPY);
+        assertDoesNotThrow(() -> account.updateBalance(1_000_000, "add"));
+        assertEquals(1_000_000, account.getBalance());
+    }
+}

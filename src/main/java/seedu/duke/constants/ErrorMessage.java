7d2abeee7b749225336e85b9512247051c0e31df
bawfen <benjamin@beepbeep.tech>
2023-03-15 19:13:48 +0800

    Add check for amount being too small for exchange. Shows exchange rate in both directions even when amount is specified

--- src/main/java/seedu/duke/constants/ErrorMessage.java
+++ src/main/java/seedu/duke/constants/ErrorMessage.java
@@ -17,17 +17,19 @@ public interface ErrorMessage {
     String INSUFFICIENT_WITHDRAW_BALANCE = "You do not have sufficient balance to withdraw.";
     String NO_AMOUNT_PROVIDED = "Please provide an amount to be added.";
 
-    String INVALID_EXCHANGE_ARGUMENT = "Please structure your exchange as " + 
+    String INVALID_EXCHANGE_ARGUMENT = "Please structure your exchange as " +
             "'exchange STARTING_CURRENCY TARGET_CURRENCY AMOUNT_IN_STARTING'";
-    String NOT_ENOUGH_IN_ACCOUNT = "Please ensure you have enough money in your" + 
+    String NOT_ENOUGH_IN_ACCOUNT = "Please ensure you have enough money in your" +
             " starting currency account to perform this transaction";
     String INVALID_NUMBER = "Please enter a valid number to exchange";
     String INVALID_DELETE_ACCOUNT_COMMAND = "Please check that you have correctly provided the " +
             "currency account to delete";
-    String ACCOUNT_NOT_EMPTY     = "Please empty your currency account before account deletion";
+    String ACCOUNT_NOT_EMPTY = "Please empty your currency account before account deletion";
     String NEGATIVE_NUMBER = "Please enter a positive number to show the rate!";
     String SHOWRATE_SYNTAX = "Please structure show-rate as 'show-rate CURRENCY CURRENCY [AMOUNT]'";
     String ACCOUNT_ALREADY_EXISTS = "You already have an account of this currency";
     String INVALID_UPDATE_BALANCE_ACTION = "The provided action for updating balance was invalid";
+    String EXCHANGE_AMOUNT_TOO_SMALL = "The amount is too small to exchange " +
+            "(e.g. does not become at least 0.01 in the target currency)";
 
 }

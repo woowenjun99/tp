fd10bea4045503be30c08c35061de21dc23083be
woowenjun99 <woowenjun99@gmail.com>
2023-03-26 23:12:10 +0800

    Write documentation and remove logging

--- src/main/java/seedu/duke/storage/Store.java
+++ src/main/java/seedu/duke/storage/Store.java
@@ -9,14 +9,12 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 
 public class Store {
     private static final String FILE_NAME = "store.json";
     private static final File file = new File(FILE_NAME);
     private static final Gson gson = new Gson();
-    private static final Logger logger = Logger.getLogger("logger");
 
     private static void createFileIfNotExist () throws IOException {
         if (file.createNewFile()) {
@@ -27,20 +25,23 @@ public class Store {
         }
     }
 
-    public static void getFromStore (AccountList accounts) {
-        try {
-            createFileIfNotExist();
-            BufferedReader br = new BufferedReader(new FileReader(FILE_NAME));
-            Storage[] store = gson.fromJson(br, Storage[].class);
-            for (Storage account: store) {
-                Currency currency = Currency.valueOf(account.getCurrency());
-                long value = account.getValue();
-                accounts.addAccount(currency, value);
-            }
-            // If the 2 lengths do not match, there is a problem.
-            assert accounts.getAllAccounts().size() == store.length;
-        } catch (Exception e) {
-            logger.log(Level.WARNING, "An unexpected error occurred!");
+    /**
+     * Populates the account list with the previously stored data in the
+     * json file.
+     *
+     * @param accounts The account list hashmap.
+     */
+    public static void getFromStore (AccountList accounts) throws Exception {
+
+        createFileIfNotExist();
+        BufferedReader br = new BufferedReader(new FileReader(FILE_NAME));
+        Storage[] store = gson.fromJson(br, Storage[].class);
+        for (Storage account : store) {
+            Currency currency = Currency.valueOf(account.getCurrency());
+            long value = account.getValue();
+            accounts.addAccount(currency, value);
         }
+        // If the 2 lengths do not match, there is a problem.
+        assert accounts.getAllAccounts().size() == store.length;
     }
 }

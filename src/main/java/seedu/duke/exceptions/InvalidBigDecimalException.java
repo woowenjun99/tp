375cae1103a4d984fe0a73a4f488e027a076a9a7
woowenjun99 <woowenjun99@gmail.com>
2023-04-02 22:16:25 +0800

    Create an exception for invalid big decimal such as more than 2 dp or more than 10000000

---
+++ src/main/java/seedu/duke/exceptions/InvalidBigDecimalException.java
@@ -0,0 +1,13 @@
+package seedu.duke.exceptions;
+
+public class InvalidBigDecimalException extends Exception {
+    private final String description;
+
+    public InvalidBigDecimalException(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+}

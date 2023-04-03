3af4111b324b4bbc458251f9f23df133d580e66d
jacob-stein1 <jmstein@bu.edu>
2023-04-04 00:16:16 +0800

    Change API key

--- src/main/java/seedu/duke/api/ExchangeRates.java
+++ src/main/java/seedu/duke/api/ExchangeRates.java
@@ -15,7 +15,7 @@ import retrofit2.Response;
 
 public class ExchangeRates {
 
-    private static final String APP_ID = "392d39a60ac5437ea539006a990d8d65";
+    private static final String APP_ID = "1349651eb52b4f85a5ced93d579652ea";
     private static final String BASE_CURRENCY = "USD";
 
     private static Map<String, Double> exchangeRatesMap;

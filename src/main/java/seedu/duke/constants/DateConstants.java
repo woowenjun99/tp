76f81a5723e2071d7a4ab55b2323030e5c4bbd3b
Arif-Khalid <arifkhalid99@gmail.com>
2023-04-06 14:56:05 +0800

    Fix checkstyle errors in DateConstants

--- src/main/java/seedu/duke/constants/DateConstants.java
+++ src/main/java/seedu/duke/constants/DateConstants.java
@@ -13,18 +13,20 @@ public interface DateConstants {
     DateTimeFormatter INPUT_DATE_FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 
     // This assumes the year is not a leap year
-    HashMap<Month, Integer> DAYS_IN_MONTH = new HashMap<>() {{
-        put(Month.JANUARY, 31);
-        put(Month.FEBRUARY, 28);
-        put(Month.MARCH, 31);
-        put(Month.APRIL, 30);
-        put(Month.MAY, 31);
-        put(Month.JUNE, 30);
-        put(Month.JULY, 31);
-        put(Month.AUGUST, 31);
-        put(Month.SEPTEMBER, 30);
-        put(Month.OCTOBER, 31);
-        put(Month.NOVEMBER, 30);
-        put(Month.DECEMBER, 31);
-    }};
+    HashMap<Month, Integer> DAYS_IN_MONTH = new HashMap<>() {
+        {
+            put(Month.JANUARY, 31);
+            put(Month.FEBRUARY, 28);
+            put(Month.MARCH, 31);
+            put(Month.APRIL, 30);
+            put(Month.MAY, 31);
+            put(Month.JUNE, 30);
+            put(Month.JULY, 31);
+            put(Month.AUGUST, 31);
+            put(Month.SEPTEMBER, 30);
+            put(Month.OCTOBER, 31);
+            put(Month.NOVEMBER, 30);
+            put(Month.DECEMBER, 31);
+        }
+    };
 }

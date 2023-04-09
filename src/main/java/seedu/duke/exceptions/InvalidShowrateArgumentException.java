cecebd2e07335cfa0ec1f8fd552aa897bd369d17
bawfen <benjamin@beepbeep.tech>
2023-04-09 13:29:49 +0800

    Remove files in wrong directory

--- src/main/java/seedu/duke/exceptions/InvalidShowrateArgumentException.java
+++
@@ -1,8 +0,0 @@
-package seedu.duke.exceptions;
-
-/**
- * An exception thrown when invalid syntax is used for the show-rate command
- */
-
-public class InvalidShowrateArgumentException extends Exception {
-}
--- src/main/java/seedu/duke/exceptions/NotEnoughInAccountException.java
+++
@@ -1,7 +0,0 @@
-package seedu.duke.exceptions;
-
-/**
- * An exception that is thrown when a user attempts to exchange more than is in their account
- */
-public class NotEnoughInAccountException extends Exception {
-}
